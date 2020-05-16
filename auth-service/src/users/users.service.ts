8925ec56f8422e2a83b714f8ebf69e10258c3052
Kamil Wronka <kamilwronka7@gmail.com>
2020-05-16 11:30:41 -0700

    Auth service

--- auth-service/src/users/users.service.ts
+++ auth-service/src/users/users.service.ts
@@ -1,20 +1,28 @@
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
 
 import { User } from './schemas/user.schema';
 import { SignUpUserDto } from 'src/auth/dto/signup-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+  constructor(
+    @InjectModel('User') private readonly userRepository: Model<User>,
+  ) {}
   async findOne(email: string): Promise<User | undefined> {
-    return this.userModel.findOne({ email });
+    return this.userRepository.findOne({ email });
   }
 
   async create(signUpUserDto: SignUpUserDto) {
-    let createdUser = new this.userModel(signUpUserDto);
+    let createdUser = new this.userRepository(signUpUserDto);
+    createdUser.password = await bcrypt.hash(createdUser.password, 10);
 
-    return await createdUser.save();
+    try {
+      return await createdUser.save();
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 }
