d7dbc54e3695131d2cec767f70246d07f9524986
Kamil Wronka <kamil.wronka@gft.com>
2020-07-15 20:02:36 +0200

    test

--- auth-service/src/config/config.ts
+++ auth-service/src/config/config.ts
@@ -6,7 +6,10 @@ export const REDIS_PORT = process.env.REDIS_PORT;
 
 export const prod = process.env.NODE_ENV === 'production';
 const DEV_WHITELIST = ['http://localhost:3000', 'http://192.168.8.130:3000'];
-const PROD_WHITELIST = ['https://dashboard.csgoed.com'];
+const PROD_WHITELIST = [
+  'http://localhost:3000',
+  'https://dashboard.csgoed.com',
+];
 
 export const CORS_OPTIONS = {
   origin: (origin, callback) => {
