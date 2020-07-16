b3bb4837a5467bcc540d2b6e0d91fe63dd4bc5b3
Kamil <kamilwronka7@gmail.com>
2020-07-16 20:03:25 +0200

    Added domain to whitelisst

--- auth-service/src/config/config.ts
+++ auth-service/src/config/config.ts
@@ -5,10 +5,15 @@ export const REDIS_HOST = process.env.REDIS_HOST;
 export const REDIS_PORT = process.env.REDIS_PORT;
 
 export const prod = process.env.NODE_ENV === 'production';
-const DEV_WHITELIST = ['http://localhost:3000', 'http://192.168.8.130:3000'];
+const DEV_WHITELIST = [
+  'http://localhost:3000',
+  'http://192.168.8.130:3000',
+  'https://dev.dashboard.kamilwronka.dev',
+];
 const PROD_WHITELIST = [
   'http://localhost:3000',
   'https://dashboard.csgoed.com',
+  'https://dev.dashboard.kamilwronka.dev',
 ];
 
 export const CORS_OPTIONS = {
