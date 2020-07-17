8abc08168a50173b327a488ae492959fcd66ea7e
Kamil Wronka <kamil.wronka@gft.com>
2020-07-17 14:21:41 +0200

    test

--- auth-service/src/users/helpers/extract-metadata-from-request.ts
+++ auth-service/src/users/helpers/extract-metadata-from-request.ts
@@ -7,7 +7,7 @@ export const extractMetadataFromRequest = async (
   req: Request,
   fingerprint: string,
 ): Promise<AuthHistory> => {
-  console.log(req.headers);
+  console.log(req.headers["'x-forwarded-for'"]);
   let ip = req.headers["'x-forwarded-for'"] || req.connection.remoteAddress;
   let device = req.headers['user-agent'];
 
